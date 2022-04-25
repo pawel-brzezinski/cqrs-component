a6e78f06b744a9513eaed9a6a545fac16225aef4
Paweł Brzeziński <pawel.brzezinski@smartint.pl>
2022-04-25 17:16:52 +0200

    Add new domain value objects: - boolean logical value object

---
+++ src/Domain/Logic/ValueObject/Boolean.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PB\Component\CQRS\Domain\Logic\ValueObject;
+
+use Assert\Assertion;
+use Assert\AssertionFailedException;
+
+/**
+ * @author Paweł Brzeziński <pawel.brzezinski@smartint.pl>
+ */
+final class Boolean
+{
+    private const ASSERTION_MSG = 'Value "%s" is not valid boolean value.';
+
+    private bool $value;
+
+    /**
+     * @param bool $value
+     */
+    private function __construct(bool $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return static
+     *
+     * @throws AssertionFailedException
+     */
+    public static function fromValue($value): self
+    {
+        $boolean = filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+        Assertion::notNull($boolean, sprintf(self::ASSERTION_MSG, $value));
+
+        return new self($boolean);
+    }
+
+    /**
+     * @return bool
+     */
+    public function dump(): bool
+    {
+        return $this->value;
+    }
+}
---
+++ tests/Domain/Logic/ValueObject/BooleanTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PB\Component\CQRS\Tests\Domain\Logic\ValueObject;
+
+use Assert\AssertionFailedException;
+use PB\Component\CQRS\Domain\Logic\ValueObject\Boolean;
+use PB\Component\FirstAid\Reflection\ReflectionHelper;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+
+/**
+ * @author Paweł Brzeziński <pawel.brzezinski@db-team.pl>
+ */
+final class BooleanTest extends TestCase
+{
+    ########################
+    # Boolean::fromValue() #
+    ########################
+
+    /**
+     * @return array
+     */
+    public function fromValueDataProvider(): array
+    {
+        return [
+            'value is real bool `true`' => [true, true, null],
+            'value is real bool `false`' => [false, false, null],
+            'value is int 1' => [1, true, null],
+            'value is int 0' => [0, false, null],
+            'value is string `1`' => ['1', true, null],
+            'value is string `0`' => ['0', false, null],
+            'value is string `true`' => ['true', true, null],
+            'value is string `false`' => ['false', false, null],
+            'value is string `on`' => ['on', true, null],
+            'value is string `off`' => ['off', false, null],
+            'value is string `yes`' => ['yes', true, null],
+            'value is string `no`' => ['no', false, null],
+            'value is string `fake`' => ['fake', null, 'Value "fake" is not valid boolean value.'],
+        ];
+    }
+
+    /**
+     * @dataProvider fromValueDataProvider
+     *
+     * @param mixed $value
+     * @param bool|null $expected
+     * @param string|null $expectedErrorMsg
+     *
+     * @return void
+     *
+     * @throws ReflectionException
+     */
+    public function testShouldCallFromValueStaticMethodAndCheckIfValueObjectHasBeenCreatedCorrectly(
+        $value,
+        ?bool $expected,
+        ?string $expectedErrorMsg
+    ): void {
+        // Expect
+        if (null !== $expectedErrorMsg) {
+            $this->expectException(AssertionFailedException::class);
+            $this->expectExceptionMessage($expectedErrorMsg);
+        }
+
+        // When
+        $actual = Boolean::fromValue($value);
+
+        // Then
+        $this->assertSame($expected, ReflectionHelper::getPropertyValue($actual, 'value'));
+    }
+
+    #######
+    # End #
+    #######
+
+    ###################
+    # Boolean::dump() #
+    ###################
+
+    /**
+     * @return void
+     *
+     * @throws AssertionFailedException
+     */
+    public function testShouldCallDumpMethodAndCheckIfReturnedBooleanIsCorrect(): void
+    {
+        // When
+        $actual = Boolean::fromValue(true);
+
+        // Then
+        $this->assertTrue($actual->dump());
+    }
+
+    #######
+    # End #
+    #######
+}
